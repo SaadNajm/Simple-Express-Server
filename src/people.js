6a3fa4bcb060b609ba9b809b25bd6fc56def2d3b
Saad Najm <saadnajm.msc@gmail.com>
2023-03-14 01:16:26 +0100

    post

---
+++ src/people.js
@@ -0,0 +1,17 @@
+export let people=[
+    {
+         name:"john",
+         age:35,
+         hairColor:"brown",
+    },
+    {
+        name:"joe",
+        age:20,
+        hairColor:"blond",
+   },
+   {
+    name:"jane",
+    age:40,
+    hairColor:"red",
+}
+]
